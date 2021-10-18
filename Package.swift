66104ceea6d8afab040b8206d4521c3d8fab406f
Max Cobb <maxxc@mac.com>
2021-10-18 10:48:23 +0200

    add patch fix for ipv6 network issue

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AgoraRtmKit",
-            url: "https://download.agora.io/swiftpm/AgoraRTM_iOS/1.4.8/AgoraRtmKit.xcframework.zip",
+            url: "https://github.com/AgoraIO/AgoraRtm_iOS/releases/download/1.4.8-230/AgoraRtmKit.xcframework.zip",
             checksum: "5bf40fe3bf10d0d05d31ff3031f49d72f338072f3dc4ba2e33a6516a7920b6b1"
         )
     ]
